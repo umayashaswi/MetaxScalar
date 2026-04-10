23a111f24bb1af9935730cdb35084979370c7fd1
Alla Uma Yashaswi <168863470+umayashaswi@users.noreply.github.com>
2026-04-10 23:22:05 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,1 +1,10 @@
-from inference import app
+from web_api import app as main_app
+import uvicorn
+
+app = main_app
+
+def main():
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+
+if __name__ == "__main__":
+    main()
